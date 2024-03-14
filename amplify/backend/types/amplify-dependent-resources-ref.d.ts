882bf13e21b04413b7c8b6ceeeab243e7457d748
EC2 Default User <ec2-user@ip-172-31-28-202.ec2.internal>
2024-03-14 22:18:45 +0000

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "viteprojects380cbc66": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
